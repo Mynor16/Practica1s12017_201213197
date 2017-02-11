a9d23343f0fa48ce8f8bd53c06b11f9876e5ecf6
Mynor16 <mynorrompi@gmail.com>
2017-02-10 21:07:08 -0600

    Primer comit de la practica 1

---
+++ src/PLSimple/NodoLSimple.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PLSimple;
+
+/**
+ *
+ * @author Mynor
+ */
+public class NodoLSimple {
+            String palabra;
+            NodoLSimple siguiente;
+ void NodoLSimple(){
+    this.palabra = new String();
+    this.siguiente = null;
+
+}
+}
---
+++ src/scrabble/Scrabble.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package scrabble;
+
+/**
+ *
+ * @author Mynor
+ */
+public class Scrabble {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Inicio inicio = new Inicio();
+        inicio.setVisible(true);
+        
+    }
+}
